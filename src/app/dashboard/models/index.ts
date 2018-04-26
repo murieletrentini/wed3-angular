d5a96290535faf8215c2dfd4f66aaaa7dc1cad61
Murièle Trentini <muriele@trentini.ch>
2018-04-26 12:46:48 +0200

    format currencies in latestTransactions

--- src/app/dashboard/models/index.ts
+++ src/app/dashboard/models/index.ts
@@ -1,4 +1,4 @@
 // TODO: Add barrel exports of your features (export * from '...';) here...
 export * from './transaction';
-export * from './accountInfo';
+export * from './account-info';
 export * from './transfer-info';
