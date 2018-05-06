7e57953904bf2e7bf15cbfda51335165226613db
Murièle Trentini <muriele@trentini.ch>
2018-05-06 08:33:39 +0200

    remove TODO's

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,8 @@ import {AuthGuard} from './auth/services/auth.guard';
 import {RegisterComponent} from './auth/components';
 
 const appRoutes: Routes = [
-  // TODO: Add routing of lazy loaded dashboard Module (with guards) here...
   {path: 'dashboard', loadChildren: 'app/dashboard/dashboard.module#DashboardModule', canLoad: [AuthGuard]},
 
-  // TODO: Add routing of eagerly loaded modules here...
   {path: 'register', component: RegisterComponent},
   {path: '', redirectTo: '/welcome', pathMatch: 'full'},
   {path: '**', redirectTo: '/welcome', pathMatch: 'full'}
